7f29d5c7e13c35819f2b6ad38268f884e02caaec
Ravikiran Reddy Balemla <ravikiranbalemla5@gmail.com>
2025-03-04 13:16:47 +0530

    Update FileManagementApplication.java

--- services/beeja-file-management/src/main/java/com/beeja/api/filemanagement/FileManagementApplication.java
+++ services/beeja-file-management/src/main/java/com/beeja/api/filemanagement/FileManagementApplication.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class FileManagementApplication {
 
   public static void main(String[] args) {
+    // change
     SpringApplication.run(FileManagementApplication.class, args);
   }
 }
