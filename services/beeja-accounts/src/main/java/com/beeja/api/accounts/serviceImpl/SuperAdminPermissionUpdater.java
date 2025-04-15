1a26cd32e5e3932e4dcd6021a733687d31d6a3b5
vijay-kumar-pitla <pitlavk@gmail.com>
2025-04-15 08:52:07 +0530

    Validate and grant missing permissions to Super Admin for full access (#169)
    This PR introduce :
    - Implemented a validation mechanism to ensure the Super Admin has all
    permissions.
    - Retrieved the complete set of permissions from the JSON file and
    compared them with the database.
    - Automatically updated the database with any missing permissions.  
    - Assigned missing permissions to the Super Admin role to maintain full
    access.
    - Integrated the update process to run asynchronously during application
    startup.
    
    ---------
    
    Co-authored-by: kirannani168 <ravikiranbalemla5@gmail.com>

---
+++ services/beeja-accounts/src/main/java/com/beeja/api/accounts/serviceImpl/SuperAdminPermissionUpdater.java
@@ -0,0 +1,81 @@
+package com.beeja.api.accounts.serviceImpl;
+
+import com.beeja.api.accounts.enums.SubscriptionName;
+import com.beeja.api.accounts.model.Organization.Role;
+import com.beeja.api.accounts.model.subscriptions.Permissions;
+import com.beeja.api.accounts.repository.PermissionRepository;
+import com.beeja.api.accounts.repository.RolesRepository;
+import com.beeja.api.accounts.utils.Constants;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.*;
+
+@Service
+@Slf4j
+public class SuperAdminPermissionUpdater {
+
+    @Autowired
+    private PermissionRepository permissionRepository;
+
+    @Autowired
+    RolesRepository rolesRepository;
+
+    @PostConstruct
+    public void initPermissionsUpdate() {
+        updatePermissions();
+    }
+
+    @Async
+    public void updatePermissions() {
+        log.info(Constants.UPDATING_PERMISSIONS_FOR_ALL_SUPER_ADMINS);
+        Permissions dbPermissions = permissionRepository.findByName(SubscriptionName.ALL_PERMISSIONS);
+
+        Set<String> jsonPermissions = getAllPermissions();
+
+        List<Role> superAdminPermissions = rolesRepository.findAllByName("Super Admin");
+
+//        TODO: Update logic to consider Subscription Plans
+            if(!dbPermissions.getPermissions().containsAll(jsonPermissions))
+            {
+                log.info(Constants.ADDING_NEW_PERMISSIONS_TO_DB);
+                dbPermissions.getPermissions().addAll(jsonPermissions);
+                permissionRepository.save(dbPermissions);
+            }
+            log.info(Constants.ADDING_NEW_PERMISSIONS_TO_SUPER_ADMINS);
+            superAdminPermissions.forEach(role -> {
+                Set<String> saPermissions = role.getPermissions();
+                if(!saPermissions.containsAll(dbPermissions.getPermissions())){
+                    saPermissions.addAll(dbPermissions.getPermissions());
+                    rolesRepository.save(role);
+                }
+            });
+            log.info(Constants.PERMISSIONS_UPDATED_SUCCESSFULLY);
+    }
+    public Set<String> getAllPermissions() {
+        log.info(Constants.GETTING_ALL_PERMISSIONS_FROM_JSON);
+            Set<String> permissionsSet = new HashSet<>();
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                ClassPathResource resource = new ClassPathResource("permissions/AllPermissions.json");
+                JsonNode rootNode = objectMapper.readTree(resource.getInputStream());
+                for (JsonNode arrayNode : rootNode) {
+                    if (arrayNode.isArray()) {
+                        arrayNode.forEach(permission -> permissionsSet.add(permission.asText()));
+                    }
+                }
+            } catch (IOException e) {
+                log.error(Constants.ERROR_READING_PERMISSIONS_JSON, e);
+                return Collections.emptySet();
+            }
+            return permissionsSet;
+    }
+}
+
