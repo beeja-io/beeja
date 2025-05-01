aa7cf5e4d1b338da5e45482219f9c489955397cc
sampath pabha <sampathpabha818@gmail.com>
2025-05-01 15:23:50 +0530

    enhancement:added backend common

---
+++ services/backend-common/src/main/java/com/beeja/common/backend_common/filters/AuthUrlProperties.java
@@ -0,0 +1,16 @@
+package com.beeja.common.backend_common.filters;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "authentication")
+public class AuthUrlProperties {
+    private String clientId;
+    private String tokenUri;
+}
