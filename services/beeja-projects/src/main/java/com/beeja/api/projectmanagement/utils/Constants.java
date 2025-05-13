8c845dc125c97081643bc59b31916c1b777ccd73
kirannani168 <ravikiranbalemla5@gmail.com>
2025-05-13 10:50:26 +0530

    enhance: added org address for user context in project service

--- services/beeja-projects/src/main/java/com/beeja/api/projectmanagement/utils/Constants.java
+++ services/beeja-projects/src/main/java/com/beeja/api/projectmanagement/utils/Constants.java
@@ -8,6 +8,7 @@ public class Constants {
   public static final String ID = "id";
   public static final String NAME = "name";
   public static final String EMAIL = "email";
+    public static final String ADDRESS = "address";
   public static final String DOC_URL = "https://docs.beeja.io/";
   public static final String BEEJA = "BEEJA";
   public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
