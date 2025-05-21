9646e0b0ba51e8d5fb52508aee2812d9a41c42e0
vijay-kumar-pitla <pitlavk@gmail.com>
2025-05-21 18:33:49 +0530

    enhance:Implemented Download API in Client Service for File Retrieval (#316)
    This PR Introduces :
    
    - Implemented a new API in the Client Service to handle file download
    requests by forwarding them to the File Service via the
       FileClient.
    - Added proper exception handling throughout the flow to ensure robust
    error management.
    - Integrated permission checks using the @HasPermission annotation to
    restrict access based on defined permissions.
    - Updated AllPermissions.json with a new permission key for this
    download API, ensuring that only authorized users (like Super
      Admin) can access it.
    - Verified the API response using Postman to confirm that the correct
    file, along with its filename and content-type, is returned
      during downloads.
    
    ---------
    
    Co-authored-by: kirannani168 <ravikiranbalemla5@gmail.com>

---
+++ services/beeja-projects/src/main/java/com/beeja/api/projectmanagement/controllers/FileController.java
@@ -0,0 +1,81 @@
+package com.beeja.api.projectmanagement.controllers;
+
+import com.beeja.api.projectmanagement.annotations.HasPermission;
+import com.beeja.api.projectmanagement.constants.PermissionConstants;
+import com.beeja.api.projectmanagement.model.dto.File;
+import com.beeja.api.projectmanagement.request.FileUploadRequest;
+import com.beeja.api.projectmanagement.responses.FileResponse;
+import com.beeja.api.projectmanagement.service.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("v1/files")
+public class FileController {
+
+    @Autowired
+    FileService fileService;
+
+    @GetMapping("download/{fileId}")
+    @HasPermission(PermissionConstants.READ_DOCUMENT)
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileId) throws Exception{
+        ByteArrayResource byteArrayResource = fileService.downloadFile(fileId);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(
+                HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + byteArrayResource.getFilename() + "\"");
+        headers.add("Access-Control-Expose-Headers", "Content-Disposition");
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .headers(headers)
+                .body(byteArrayResource);
+    }
+
+    @GetMapping("/{entityId}")
+    @HasPermission({
+            PermissionConstants.READ_DOCUMENT,
+            PermissionConstants.READ_ALL_DOCUMENTS
+    })
+    public ResponseEntity<FileResponse> getAllFilesOfEntityId(
+            @PathVariable String entityId,
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size)
+            throws Exception {
+        return ResponseEntity.ok(fileService.listOfFileByEntityId(entityId, page, size));
+    }
+
+    @PostMapping
+    @HasPermission({
+            PermissionConstants.CREATE_DOCUMENT,
+            PermissionConstants.CREATE_ALL_DOCUMENT
+    })
+    public ResponseEntity<File> uploadFile(FileUploadRequest fileUploadRequest) throws Exception {
+        return ResponseEntity.ok(fileService.uploadFile(fileUploadRequest));
+    }
+
+    @DeleteMapping("/{fileId}")
+    @HasPermission({
+            PermissionConstants.DELETE_DOCUMENT,
+            PermissionConstants.DELETE_ALL_DOCUMENT
+    })
+    public ResponseEntity<File> deleteFile(@PathVariable String fileId) throws Exception {
+        return ResponseEntity.ok(fileService.deleteFile(fileId));
+    }
+
+    @PutMapping("/{fileId}")
+    @HasPermission({
+            PermissionConstants.UPDATE_DOCUMENT,
+            PermissionConstants.UPDATE_ALL_DOCUMENT
+    })
+    public ResponseEntity<File> updateFileByFileId(
+            @PathVariable String fileId, FileUploadRequest fileUploadRequest) throws Exception {
+        return ResponseEntity.ok(fileService.updateFile(fileId, fileUploadRequest));
+    }
+
+}
