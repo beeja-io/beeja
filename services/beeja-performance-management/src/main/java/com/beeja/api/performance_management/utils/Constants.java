1f39379f565d4436c0360f934ecd96f9069ac608
kirannani168 <ravikiranbalemla5@gmail.com>
2025-05-28 11:37:59 +0530

    service: added new service for performance management

---
+++ services/beeja-performance-management/src/main/java/com/beeja/api/performance_management/utils/Constants.java
@@ -0,0 +1,14 @@
+package com.beeja.api.performance_management.utils;
+
+public class Constants {
+    public static final String NAME = "name";
+    public static final String EMAIL = "email";
+    public static final String AUTHORIZATION = "authorization";
+    public static final String ID = "id";
+
+    public static final String USER_FAILED_AUTHENTICATE = "User failed to authenticate";
+    public static final String ACCESS_DENIED = "Access denied";
+    public static final String DOC_URL_RESOURCE_NOT_FOUND = "https://docs.beeja.io/docs/resource-not-found";
+    public static final String BEEJA = "BEEJA";
+    public static final String NO_REQUIRED_PERMISSIONS = "You do not have the required permissions to access this resource";
+}
