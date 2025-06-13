1d4fadeb1bca97b4e0eeab3a423dd2b835c39ea9
sampath pabha <sampathpabha818@gmail.com>
2025-06-13 17:00:39 +0530

    enhance:Added loanLimit to OrgValues entity

--- services/beeja-accounts/src/main/java/com/beeja/api/accounts/model/Organization/employeeSettings/OrgValues.java
+++ services/beeja-accounts/src/main/java/com/beeja/api/accounts/model/Organization/employeeSettings/OrgValues.java
@@ -1,5 +1,6 @@
 package com.beeja.api.accounts.model.Organization.employeeSettings;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,10 +9,13 @@ import org.springframework.data.mongodb.core.mapping.Field;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class OrgValues {
 
   @Field("name")
   private String value;
 
   private String description;
+
+  private String loanLimit;
 }
