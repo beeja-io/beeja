8413038f0dff69b2ee85eebdd5b287df26a31289
tac-hari <haritechatcore@gmail.com>
2025-10-27 11:04:49 -0400

    Update Constants.java

--- services/beeja-cloud-gateway/src/main/java/com/beeja/api/apigateway/utils/Constants.java
+++ services/beeja-cloud-gateway/src/main/java/com/beeja/api/apigateway/utils/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
   public static final String REGISTERED_USERNAME_PASSWORD_PROVIDER =
       "Registered User Name Password Auth Provider";
   public static final String ERROR_MISSING_FE_URLS = "Missing Frontend Urls for CORS"; 
+
 }
