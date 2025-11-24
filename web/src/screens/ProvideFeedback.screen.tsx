c73d83e523b00de48b54ba7362a7ac7f7b2948c3
Praneeth1404 <praneethbeldari1404@gmail.com>
2025-11-24 13:40:42 +0530

    fixed the onclick function for the provide feedback

--- web/src/screens/ProvideFeedback.screen.tsx
+++ web/src/screens/ProvideFeedback.screen.tsx
@@ -454,7 +454,7 @@ const ProvideFeedback: React.FC<ProvideFeedbackProps> = ({
               <ProvideButton
                 disabled={item.submitted || isPermissionDenied}
                 onClick={() => {
-                  if (!item.submitted && isPermissionDenied) {
+                  if (!item.submitted && !isPermissionDenied) {
                     handleProvideFeedback(item);
                   }
                 }}
